76aecc8f266007ff76f8def3b81fb0662e0ba411
Pierre <jarzynkapierre07@gmail.com>
2022-12-01 22:01:44 +0100

    Creation

---
+++ index.py
@@ -0,0 +1,7 @@
+from PIL import Image  # Python Image Library - Image Processing
+import glob
+
+
+im = Image.open("th.png")  #Png go there
+rgb_im = im.convert('RGB')
+rgb_im.save('colors.jpg') # result
